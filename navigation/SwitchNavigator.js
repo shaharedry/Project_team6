c8d872a39d9fd39246c65af23a93dc5c6c6e4d38
Harel <harelelihu@gmail.com>
2021-04-05 22:20:41 +0300

    Hackaton

---
+++ navigation/SwitchNavigator.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+import Login from '../screens/Login'
+import Signup from '../screens/Signup'
+import Logged from '../screens/Logged'
+
+const SwitchNavigator = createSwitchNavigator(
+    {
+        Login: {
+            screen: Login
+        },
+
+        Signup: {
+            screen: Signup
+        },
+
+        Logged: {
+            screen: Logged
+        }
+
+    },
+    {
+        initialRouteName: 'Login'
+    }
+)
+
+export default createAppContainer(SwitchNavigator)
