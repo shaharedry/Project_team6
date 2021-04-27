eed28775dd0f72998ca20f305d06ed1f969b435b
Shahar <Shahared1907@gmail.com>
2021-04-28 02:26:08 +0300

    update

--- screens/Teacher/EnterPresence.js
+++ screens/Teacher/EnterPresence.js
@@ -1,5 +1,3 @@
-export default (EnterPresence)
-
 import React, { useCallback, useContext, useEffect, useState } from 'react';
 import {View, Text, StyleSheet ,Button, Alert , TouchableOpacity , Keyboard} from 'react-native';
 import colors from '../../constants/Colors';
