e7bd2df9eccaf1dbce3c726d40261c8f8084b0c6
Shahar <Shahared1907@gmail.com>
2021-04-28 00:20:54 +0300

    test

---
+++ test/app.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from '../App';
+import FirstScreen from "../screens/FirstScreen";
+
+/*test('Open first screen', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).dispatch(FirstScreen);
+});
+
+it('Test click event', () => {
+    const mockCallBack = jest.fn();
+
+    const button = shallow((<Button onClick={mockCallBack}>Ok!</Button>));
+    button.find('button').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+});*/
+
+test('renders correctly across screens', () => {
+    const tree = renderer.create().toJSON();
+    expect(tree).toMatchSnapshot();
+});
